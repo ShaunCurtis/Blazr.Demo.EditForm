ff5a2e8482edf9aff674d8369e00cbcba1b2cf41
Shaun Curtis <shaun@coldelm.com>
2022-11-12 08:34:05 +0000

    Update

--- Blazr.Demo.EditForm/Blazr.Demo.EditForm.Core/Entities/WeatherForecast/DataClasses/WeatherForecastEditContext.cs
+++ Blazr.Demo.EditForm/Blazr.Demo.EditForm.Core/Entities/WeatherForecast/DataClasses/WeatherForecastEditContext.cs
@@ -1,3 +1,7 @@
+
+using Microsoft.AspNetCore.Components.Forms;
+
+using System.ComponentModel.DataAnnotations;
 /// ============================================================
 /// Author: Shaun Curtis, Cold Elm Coders
 /// License: Use And Donate
@@ -6,10 +10,95 @@
 
 namespace Blazr.Demo.EditForm.Core
 {
-    public class DeoWeatherForecast
+    public class WeatherForecastEditContext
     {
-        public Guid Id { get; set; } = GuidExtensions.Null;
+        private Guid _newId = Guid.NewGuid();
 
+        private Guid _uid = Guid.Empty;
+        public override Guid Uid
+        {
+            get => _uid;
+            set => SetIfChanged(ref _uid, value, WeatherForecastConstants.Uid);
+        }
+
+        private Guid _ownerId = Guid.Empty;
+        public Guid OwnerId
+        {
+            get => _ownerId;
+            set => SetIfChanged(ref _uid, value, WeatherForecastConstants.OwnerId);
+        }
+
+        private Guid _summaryId = Guid.Empty;
+        public Guid SummaryId
+        {
+            get => _summaryId;
+            set => SetIfChanged(ref _uid, value, WeatherForecastConstants.SummaryId);
+        }
+
+        private Guid _locationId = Guid.Empty;
+        public Guid LocationId
+        {
+            get => _locationId;
+            set => SetIfChanged(ref _uid, value, WeatherForecastConstants.LocationId);
+        }
+
+        private DateTimeOffset _date;
+        public DateTimeOffset Date
+        {
+            get => _date;
+            set => SetIfChanged(ref _date, value, WeatherForecastConstants.Date);
+        }
+
+        private int _temperatureC;
+
+        public int TemperatureC
+        {
+            get => _temperatureC;
+            set => SetIfChanged(ref _temperatureC, value, WeatherForecastConstants.Date);
+        }
+
+        public WeatherForecastEditContext() { }
+
+        public WeatherForecastEditContext(DboWeatherForecast record) : base(record) { }
+
+        public override void Load(DboWeatherForecast record)
+        {
+            this.BaseRecord = record with { };
+            _uid = record.Uid;
+            _ownerId = record.OwnerId;
+            _summaryId = record.WeatherSummaryId;
+            _locationId = record.WeatherLocationId;
+            _date = record.Date;
+            _temperatureC = record.TemperatureC;
+        }
+        public override void Reset()
+            => this.Load(this.BaseRecord with { });
+
+        public override DboWeatherForecast AsNewRecord()
+            => Record with { Uid = _newId };
+
+        public override DboWeatherForecast Record
+            => new DboWeatherForecast
+            {
+                Uid = _uid,
+                OwnerId = _ownerId,
+                WeatherSummaryId = _summaryId,
+                WeatherLocationId = _locationId,
+                Date = _date,
+                TemperatureC = _temperatureC
+            };
+
+        public override ValidationResult Validate(string? fieldname = null)
+        {
+            var result = WeatherForecastValidator.Validate(this.Record, ValidationMessages, fieldname);
+            this.NotifyValidationStateUpdated(result.IsValid, fieldname);
+            return result;
+        }
+    }
+
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+        private 
         public DateTime Date { get; set; }
 
         public int TemperatureC { get; set; }
